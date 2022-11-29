031456378527e2077788bd93e55a7cd6cd347a42
hhwhhw <1171604288@qq.com>
2022-11-29 17:11:04 +0800

    module请求参数有误

--- client/inject/game/basics.ts
+++ client/inject/game/basics.ts
@@ -13,19 +13,22 @@ module HHW {
         return !!window['G'] && !!window['mo'] && !!window['G'].loginMgr;
     }
 
-    export function reqHHW(modules: string, method: string, args: any, cb?: (rst) => void) {
+    export function reqHHW(module: string, method: string, args: any, cb?: (rst) => void) {
         if (!args) args = {};
         args.hhw_team = teamX;
         args.hhw_gsIdx = getGsGrpId();
         
         postReq({
-            modules,
+            module,
             method,
             args
         }, (err, rst) => {
             if (err) sendMessToDevTool(err, 1);
+            else if (rst.err) {
+                sendMessToDevTool(rst.err, 1);
+            }
             else if (cb) {
-                cb(rst);
+                cb(rst.data);
             }
         });
     }
